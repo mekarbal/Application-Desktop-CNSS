bf33494dfa9f1b655fc82982bc64ace23e5b8737
mElhadki <elhadkimariem3@gmail.com>
2021-04-15 23:28:08 +0000

    done

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -5,13 +5,6 @@ const Login = document.getElementById("Login");
 const Register = document.getElementById("Register");
 
 
-sign_up_btn.addEventListener("click", () => {       container.classList.add("sign-up-mode");
-}); 
-
-sign_in_btn.addEventListener("click", () => {       container.classList.remove("sign-up-mode");
-});
-
-
 //login emplyee
 Login.addEventListener('click', () => {
 
@@ -31,7 +24,6 @@ Login.addEventListener('click', () => {
         .then(function (response){   
             
             window.localStorage.setItem('x-auth-token', response.data);
-            
             location.replace("DashbardEm.html")
         })
 
@@ -62,39 +54,5 @@ Login.addEventListener('click', () => {
 })
 
 
-
-
-//register Employee
-
-Register.addEventListener('click', () => {
-
-    var nameR = document.getElementById('nameR').value;
-    var emailR = document.getElementById('emailR').value;
-    var phoneR = document.getElementById('phoneR').value;
-    var mat = document.getElementById('mat').value;
-    var passwordR = document.getElementById('password').value;
-        
-        axios.post('http://localhost:5000/employee/',{
-
-            name : nameR,
-            email : emailR,
-            phone : phoneR,
-            mat : mat,
-            password : passwordR
-        })
-        .then(function (response){   
-            
-            location.replace("index.html");
-           
-
-            const myNotification = new Notification('Notification', {
-                body: 'You are Added You need just to login'
-              })
-        })
-
-        .catch(function (err) {
-            console.log(err);
-        });
-})
 
 
